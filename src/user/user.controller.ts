de7f982a57464a279e5235c38e2a78e88c691027
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-09-16 12:36:34 -0300

    feat: create user strucute

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { type AuthenticatedRequest } from 'src/auth/types/authenticated-request';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserResponseDto } from './dto/user-response.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findOne(@Req() req: AuthenticatedRequest, @Param('id') id: string) {
+    return id;
+  }
+
+  @Post()
+  async create(@Body() dto: CreateUserDto) {
+    const user = await this.userService.create(dto);
+    return new UserResponseDto(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  async update(@Req() req: AuthenticatedRequest, @Body() dto: UpdateUserDto) {
+    const user = await this.userService.update(req.user.id, dto);
+    return new UserResponseDto(user);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,75 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from 'src/common/hashing/hashing.service';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+  async findOneByOrFail(userData: Partial<User>) {
+    const user = await this.userRepository.findOneBy(userData);
+
+    if (!user) throw new BadRequestException('User not found');
+
+    return user;
+  }
+
+  async create(dto: CreateUserDto) {
+    await this.findOneByEmailExist(dto.email);
+
+    const hashedPassword = await this.hashingService.hash(dto.password);
+
+    const newUser: CreateUserDto = {
+      name: dto.name,
+      email: dto.email,
+      password: hashedPassword,
+    };
+
+    const created = await this.userRepository.save(newUser);
+    return created;
+  }
+
+  findByEmail(email: string) {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  findById(id: string) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  save(user: User) {
+    return this.userRepository.save(user);
+  }
+
+  async findOneByEmailExist(email: string) {
+    const exist = await this.userRepository.existsBy({ email });
+
+    if (exist) throw new ConflictException('E-mail already exist');
+  }
+
+  async update(id: string, dto: UpdateUserDto) {
+    if (!dto.email && !dto.name) throw new BadRequestException('Invalid data');
+
+    const user = await this.findOneByOrFail({ id });
+    user.name = dto.name ?? user.name;
+
+    if (dto.email && dto.email !== user.email) {
+      await this.findOneByEmailExist(dto.email);
+      user.email = dto.email;
+      user.forceLogout = true;
+    }
+
+    return this.save(user);
+  }
+}
