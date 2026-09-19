4b4ec989d390573642979017a9d08c9aeb3938e5
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-09-19 14:09:20 -0300

    feat: Implement cors origin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { parseCorsWhitelist } from './common/utils/parse-cors-whitelist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: { policy: 'cross-origin' },
+    }),
+  );
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined,
+      callback: (...args: any[]) => void,
+    ) => {
+      const whitelist = parseCorsWhitelist(
+        process.env.WHITELIST_RAW_STRING || '',
+      );
+      const isAllowed = origin ? whitelist.includes(origin) : false;
+      if (!origin || isAllowed) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'), false);
+    },
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
